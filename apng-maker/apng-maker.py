3993f952e56202afb0908b4737c8ad881d5f2bdd
Ozzymops <39087842+Ozzymops@users.noreply.github.com>
2024-06-27 18:57:21 +0200

    added batch support to apng-maker

--- apng-maker/apng-maker.py
+++ apng-maker/apng-maker.py
@@ -18,6 +18,21 @@ for image in os.listdir(".\\frames\\"):
     if image.endswith(accepted_filetypes):
         frames.append(os.getcwd() + "\\frames\\" + image)
 
+# Sort into dictionary
+# Basename: [image1, image2, image3]
+frameList = {}
+
+for frame in frames:
+    framename = (os.path.splitext(os.path.basename(frame))[0]).split('_', 1)[0]
+    if framename in frameList:
+        frameList[framename].append(frame)
+    else:
+        tempList = [frame]
+        frameList[framename] = tempList
+
 # Convert to apng
-filename = (os.path.splitext(os.path.basename(frames[0]))[0]).split('_', 1)[0]
-APNG.from_files(frames, delay=int(delay)).save(filename + ".png")
+for key in frameList:
+    print(key)
+    for value in frameList[key]:
+        print("- " + value)
+    APNG.from_files(frameList[key], delay=int(delay)).save(key + ".png")
--- apng-maker/readme.md
+++ apng-maker/readme.md
@@ -1,4 +1,5 @@
-Converts image files in 'frames' to single .apng file
+Converts image files in 'frames' to .apng files by filename
+frames MUST be named as 'filename_amount' e.g. 'EpicPicture_1', 'EpicPicture_2'
 delay is measured in milliseconds
 
 requirements:
